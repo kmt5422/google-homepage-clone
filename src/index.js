281abfa7f3caddf02913e42d10f32fa367006f80
Kevin Thompson <kevinthompson.nmtcs@gmail.com>
2020-06-26 18:47:59 -0400

    initial commit for remade project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+(function(){
+    const div = document.querySelector('.content');
+    const h1 = document.createElement('h1');
+
+    h1.textContent = 'Hello World';
+    div.appendChild(h1);
+})();
